ce0827ee641c8700b3f9de4fa9fe15c52361b275
NOMINMAX <48073498+YessionCC@users.noreply.github.com>
2021-08-26 14:21:05 +0800

    Update build.py

--- layers/build.py
+++ layers/build.py
@@ -1,6 +1,9 @@
 import torch
 from .perceptual_loss import Perceptual_loss
-
+from .temporal_loss import Temporal_loss
 
 def make_loss():
     return Perceptual_loss()
+
+def make_temp_loss():
+    return Temporal_loss()
