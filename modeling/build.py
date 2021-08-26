94cfa00f58968540630b92c3dcb85f0fbca57ecd
NOMINMAX <48073498+YessionCC@users.noreply.github.com>
2021-08-26 14:44:13 +0800

    Update build.py

--- modeling/build.py
+++ modeling/build.py
@@ -11,7 +11,7 @@ from .generatic_model import Generatic_Model
 
 def build_model(cfg, vertex_list, dataset = None):
 
-    model = Generatic_Model(cfg.INPUT.SIZE_TRAIN[0], cfg.INPUT.SIZE_TRAIN[1], cfg.INPUT.LAYER_NUM, cfg.DEPTH_THR,
+    model = Generatic_Model(cfg.INPUT.SIZE_TRAIN[0], cfg.INPUT.SIZE_TRAIN[1], cfg.INPUT.LAYER_NUM, cfg.WEIGHTS.DEPTH_THR,
                  vertex_list, cfg.MODEL.FEATURE_DIM, dataset =dataset, use_rgb = cfg.INPUT.USE_RGB)
 
     return model
